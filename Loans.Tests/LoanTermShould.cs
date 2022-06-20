2a90b15b39f5451f4b01b1ba76330ecfdef6ebc8
SamiWilliamsZuto <samantha.williams@zuto.com>
2022-06-20 09:59:46 +0100

    Initial commit for Intro module

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,29 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            //sut = system under test
+            var sut = new LoanTerm(1);
+
+            var numberOfMonths = sut.ToMonths();
+
+            Assert.That(numberOfMonths, new EqualConstraint(12));
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);
+            
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+    }
+}
